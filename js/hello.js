8f19d3ff3057c5aa2201eec85b7ff2f5e4d8b109
AnvitK <anvitkamble@gmail.com>
2022-12-03 17:02:50 +0530

    added conditonal statements and loops statements

--- js/hello.js
+++ js/hello.js
@@ -65,3 +65,73 @@ compareUnstrict(30, 30);
 compareStrict(30, '30');
 compareStrict(0, false);
 compareStrict(null, undefined);
+ 
+const calculateGrade1 = (marks) => {
+    if(marks >= 90) console.log(marks, 'A');
+    else if(marks >= 75 && marks < 90) console.log(marks, 'B');
+    else if(marks >= 60 && marks < 75) console.log(marks, 'C');
+    else if(marks >= 50 && marks < 60) console.log(marks, 'D');
+    else if(marks < 50) console.log(marks, 'E');
+    else console.log(marks, 'Failed');
+}
+
+const dayNo = (no) => {
+    switch(no) { 
+        case 1:
+            console.log(no, 'Monday');
+            break;
+        case 2:
+            console.log(no, 'Tuesday');
+            break;
+        case 3:
+            console.log(no, 'Wednesday');
+            break;
+        case 4:
+            console.log(no, 'Thursday');
+            break;
+        case 5:
+            console.log(no, 'Friday');
+            break;
+        case 6:
+            console.log(no, 'Saturday');
+            break;
+        case 7:
+            console.log(no, 'Sunday');
+            break;
+        default: console.log(no, 'Wrong No');
+    }
+}
+
+calculateGrade1(35);
+calculateGrade1(55);
+calculateGrade1(85);
+calculateGrade1(60);
+
+dayNo(1);
+dayNo(2);
+dayNo(6);
+dayNo(7);
+dayNo(5);
+dayNo(-1);
+dayNo(10);
+
+let i = 1;
+while(i < 11) {
+    console.log(i);
+    i++;
+}
+
+i = 15;
+do {
+    console.log(i);
+    i++;
+} while(i < 10)
+
+for(let j = 0; j < 5; j++) {
+    console.log('JS is the best!!!');
+}
+
+const arr = [11,22,33,44,55];
+arr.forEach((ele) => {
+    console.log(ele);
+})
